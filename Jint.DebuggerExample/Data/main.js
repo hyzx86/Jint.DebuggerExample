a00db347d75b280485187488367869e45a82880b
hyzx86 <huarui2219@qq.com>
2023-10-16 21:10:24 +0800

    useNewApi 新版本已不支持从相对路径导入脚本

--- Jint.DebuggerExample/Data/main.js
+++ Jint.DebuggerExample/Data/main.js
@@ -1,4 +1,15 @@
-﻿import test from "./example-module.js";
+﻿//import test from "example-module.js";
+class Test {
+    init(prefix) {
+        this.prefix = prefix;
+    }
 
+    run() {
+        this.values = [];
+        for (let i = 0; i < 10; i++) {
+            this.values.push(this.prefix + i);
+        }
+    }
+}
 test.init("Hello world!");
 test.run();
